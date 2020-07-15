aa982b74b4807e9c0998d752d2a6ebbc7fd479ea
Aryan Chauhan <sansarya2121@gmail.com>
2020-07-16 04:40:57 +0530

    base project complete code

---
+++ app/src/main/java/com/leotarius/pokespawn/ImageArFragment.kt
@@ -0,0 +1,78 @@
+package com.leotarius.pokespawn
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.ar.core.AugmentedImageDatabase
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.sceneform.ux.ArFragment
+import java.io.IOException
+
+private const val REQUEST_CODE_CHOSEN_IMAGE = 0
+
+class ImageArFragment : ArFragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        chooseNewImage()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+        arSceneView.planeRenderer.isEnabled = false
+    }
+
+    override fun getSessionConfiguration(session: Session?): Config {
+        val config = super.getSessionConfiguration(session)
+        config.focusMode = Config.FocusMode.AUTO
+        return config
+    }
+
+    private fun chooseNewImage(){
+        Intent(Intent.ACTION_GET_CONTENT).run {
+            type = "image/"
+            startActivityForResult(this, REQUEST_CODE_CHOSEN_IMAGE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==Activity.RESULT_OK && requestCode== REQUEST_CODE_CHOSEN_IMAGE){
+            val imageUri = data?.data ?: return
+            val session = arSceneView.session ?:return
+            val config = getSessionConfiguration(session)
+            val database = createImageDatabaseWithSingleImage(session, imageUri)
+            config.augmentedImageDatabase = database
+            config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+            session.configure(config)
+        }
+    }
+
+    private fun createImageDatabaseWithSingleImage(session: Session?, imageUri: Uri): AugmentedImageDatabase {
+        val database = AugmentedImageDatabase(session)
+        val bmp = loadAugumentedImageBitmap(imageUri)
+        database.addImage("myImage.jpeg", bmp)
+        return database
+    }
+
+    private fun loadAugumentedImageBitmap(imageUri: Uri): Bitmap? {
+        return try{
+            context?.contentResolver?.openInputStream(imageUri).use {
+                BitmapFactory.decodeStream(it)
+            }
+        } catch (e: IOException){
+            Log.e("Fragment", "loadAugumentedImageBitmap: IoException", e)
+            null
+        }
+    }
+}
